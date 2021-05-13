fe571413ba02c72bd660b0b2d20ca43edc1e68b2
weitao <fwtt20071028@126.com>
2021-05-14 04:23:24 +1000

    fix six

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sudo chmod +x /etc/upip/exec
 
 sudo apt-get install python3-pip
 sudo python3 -m pip install pygsheets
+sudo python3 -m pip install --upgrade six
 
 sudo systemctl daemon-reload
 sudo systemctl start upip.service
